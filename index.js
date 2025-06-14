37f15741cea3e1dd772dcb04fdea72eb1d684e50
ROSHNEE_GOUDA <120122742+ROSHNEEGOUDA@users.noreply.github.com>
2025-06-14 23:34:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const sql = require('mssql');
+const app = express();
+const PORT = 3000;
+
+const dbConfig = {
+  user: 'LAPTOP-3EO4LDAD\Saurav Kumar Singh',
+  password: 'Spiderman@6307211373',
+  server: 'LAPTOP-3EO4LDAD', // e.g. localhost or 192.168.1.x
+  database: 'MyAppDB',
+  options: {
+    trustServerCertificate: true,
+  }
+};
+
+app.post('/trigger-cube-job', async (req, res) => {
+  try {
+    await sql.connect(dbConfig);
+    await sql.query(`EXEC msdb.dbo.sp_start_job N'DeployCubeJob'`);
+    res.send({ status: 'success', message: 'Job triggered.' });
+  } catch (err) {
+    res.status(500).send({ status: 'error', message: err.message });
+  }
+});
+
+app.listen(PORT, () => console.log(`API running on port ${PORT}`));
